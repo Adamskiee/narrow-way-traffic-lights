67081972e3304fc33693085a296bacc95613c269
Adamskiee <tosinomatthew@gmail.com>
2025-11-20 16:07:17 +0800

    setup tailwindcss via tailwind cli and flowbite via cdn

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,8 @@ require_once __DIR__ . "/config.php";
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?= $page_title ?></title>
-<link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css">
+<link href="../dist/css/output.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.css" rel="stylesheet" />
 
 </head>
 <body>
