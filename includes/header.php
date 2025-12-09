3478c32f56cc513e4aa3affc2cb2adc97b661c4f
Adamskiee <tosinomatthew@gmail.com>
2025-12-09 17:56:12 +0800

    feat: add logo SVG and update favicon link in header

--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,7 @@ require_once __DIR__ . "/config.php";
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="<?= BASE_URL ?>/custom.css">
 <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css?v1">
+<link rel="icon" type="image/svg+xml" href="<?= BASE_URL ?>/assets/images/logo.svg">
 
 </head>
 <body>
